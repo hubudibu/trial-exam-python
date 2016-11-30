a99f3921ba25fb3b6133409daf2f9e1960f62f5c
matheb <mathebogi@gmail.com>
2016-11-30 10:28:38 +0100

    2.py counts 'a' letter in filename, it file exist

--- 2.py
+++ 2.py
@@ -1,3 +1,14 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file does not exist, just return 0.
+import os.path
+
+def count_as(filename):
+    counter = 0
+    if os.path.exists(filename):
+        for i in filename:
+            if i == 'a':
+                counter += 1
+    return counter
+
+print(count_as('korte.txt'))
